19fa335d67e17751bbc58cf2ad4c65b1963446ba
Hans <741581879@qq.com>
2020-07-22 17:57:20 +0800

    [Story Number][Hans]feature: Complete Practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    protected Integer klass;
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if(klass == null) {
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        else {
+            return super.introduce() + " I am a Teacher. I teach Class " + klass + ".";
+        }
+    }
 }

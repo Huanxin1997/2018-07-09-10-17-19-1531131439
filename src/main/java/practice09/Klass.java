73d842af1772b874312e983907fbcd933f5e29bc
Hans <741581879@qq.com>
2020-07-22 20:22:52 +0800

    [Story Number][Hans]feature: Add Append Members Method.

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,52 @@
 package practice09;
 
+import java.util.List;
+
 public class Klass {
+    protected int number;
+    protected Student leader;
+    protected Student student;
+
+    public Klass(int klass) {
+        this.number = klass;
+    }
+
+//    public Klass(int number, Student leader, List<Student> list) {
+//        this.number = number;
+//        this.leader = leader;
+//        this.list = list;
+//    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student student) {
+        this.leader = student;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if(this.student == null) {
+            System.out.println("It is not one of us.");
+        } else {
+            setLeader(student);
+        }
+    }
+
+    public void appendMember(Student student) {
+        this.student = student;
+    }
+
 }

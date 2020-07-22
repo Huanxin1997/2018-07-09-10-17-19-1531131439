e5ca3c3e4d1f62ecb46ff78e782073e48f27e092
Hans <741581879@qq.com>
2020-07-22 18:02:16 +0800

    [Story Number][Hans]feature: Complete Practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,21 @@
 package practice07;
 
 public class Klass {
+    protected int number;
+
+    public Klass(int klass) {
+        this.number = klass;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
 }
